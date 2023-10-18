608d3630a6d0a0a1823499a3efc0d92c5a6d786b
Erwan ROBIN <erobin.pro@outlook.fr>
2023-10-18 08:38:02 +0200

    First comit of P10 Revamp

---
+++ src/components/EventCard/index.js
@@ -0,0 +1,80 @@
+import PropTypes from "prop-types";
+import { getMonth } from "../../helpers/Date";
+
+import "./style.scss";
+
+const EventCard = ({
+  imageSrc,
+  imageAlt,
+  date = new Date(),
+  title,
+  label,
+  small = false,
+  ...props
+}) => (
+  <div
+    data-testid="card-testid"
+    className={`EventCard${small ? " EventCard--small" : ""}`}
+    {...props}
+  >
+    <div className="EventCard__imageContainer">
+      <img data-testid="card-image-testid" src={imageSrc} alt={imageAlt} />
+      <div className="EventCard__label">{label}</div>
+    </div>
+    <div className="EventCard__descriptionContainer">
+      <div className="EventCard__title">{title}</div>
+      <div className="EventCard__month">{getMonth(date)}</div>
+    </div>
+  </div>
+);
+
+
+
+// BUGSORT
+EventCard.propTypes = {
+  /* imageSrc: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired, */
+  imageSrc: PropTypes.string,
+  title: PropTypes.string,
+  label: PropTypes.string,
+  imageAlt: PropTypes.string,
+  date: PropTypes.instanceOf(Date).isRequired,
+  small: PropTypes.bool,
+};
+
+// BUGSORT
+EventCard.defaultProps = {
+  imageSrc: "Img did not load",
+  title: "title did not load",
+  label: "Label did not load",
+  imageAlt: "image",
+  small: false,
+}
+
+export default EventCard;
+
+
+
+
+
+
+
+
+
+
+
+/* 
+EventCard.propTypes = {
+  imageSrc: PropTypes.string.isRequired,
+  imageAlt: PropTypes.string,
+  date: PropTypes.instanceOf(Date).isRequired,
+  title: PropTypes.string.isRequired,
+  small: PropTypes.bool,
+  label: PropTypes.string.isRequired,
+};
+
+EventCard.defaultProps = {
+  imageAlt: "image",
+  small: false,
+} */
